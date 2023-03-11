6ba579b3813e833563229ae4140dd23da34ab031
AnnaLipenberga <124579305+AnnaLipenberga@users.noreply.github.com>
2023-03-11 21:27:45 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,5 +1,19 @@
 # python3
 
+def heapify(data, n, i, swaps):
+    smallest = i
+    left = 2*i+1
+    right = 2*i+2
+    
+    if left < n and data[left] < data[smallest]:
+        smallest = left
+    if right < n and data[right] < data[smallest]:
+        smallest = right
+    if smallest != i:
+        data[i], data[smallest] = data[smallest], data[i]
+        swaps.append((i, smallest))
+        heapify(data, n, smallest, swaps)
+        
 
 def build_heap(data):
     swaps = []
