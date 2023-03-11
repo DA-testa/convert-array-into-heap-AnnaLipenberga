588ec081db4a7a2be323f2733b21c12764ca8ac3
AnnaLipenberga <124579305+AnnaLipenberga@users.noreply.github.com>
2023-03-11 21:35:10 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -50,7 +50,7 @@ def main():
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
-    assert 0 <= len(swas) <= 4*n
+    assert 0 <= len(swaps) <= 4*n
 
 
     # output all swaps
